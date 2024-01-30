f76e1caf168f6ed6ebef93d5c71a40d9ba944326
EvgeniyKhriapa <house28101989@gmail.com>
2024-01-30 06:50:03 +0200

    Add cart views

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -4,41 +4,24 @@ from shop.models import Product
 
 
 class Cart:
-    def __int__(self, request):
-        """Ініціалізувати кошик"""
+    def __init__(self, request):
+        """
+        Initialize the cart.
+        """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            # Створити пустий кошик в сеансі
+            # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-    def add(self, product, quantity=1, override_quantity=False):
-        """Додати товар в кошик або змінити його кількість"""
-        product_id = str(product.id)
-        if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
-        if override_quantity:
-            self.cart[product_id]['quantity'] = quantity
-        else:
-            self.cart[product_id]['quantity'] += quantity
-        self.save()
-
-    def save(self):
-        """Відзначити сеанс, як змінений, щоб виконати збереження"""
-        self.session.modified = True
-
-    def remove(self, product):
-        """Видалити товар із кошика"""
-        product_id = str(product.id)
-        if product_id in self.cart:
-            del self.cart[product_id]
-            self.save()
-
     def __iter__(self):
-        """Прокрутити товарні позиції в циклі та отримати товари із бази даних"""
+        """
+        Iterate over the items in the cart and get the products
+        from the database.
+        """
         product_ids = self.cart.keys()
-        # Отримати об'єкти product та додати їх до кошика
+        # get the product objects and add them to the cart
         products = Product.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
         for product in products:
@@ -49,14 +32,42 @@ class Cart:
             yield item
 
     def __len__(self):
-        """Підрахувати всі позиції в кошику"""
+        """
+        Count all items in the cart.
+        """
         return sum(item['quantity'] for item in self.cart.values())
 
-    def get_total_price(self):
-        """Підрахунок загальної вартості товарів у кошику"""
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+    def add(self, product, quantity=1, override_quantity=False):
+        """
+        Add a product to the cart or update its quantity.
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        # mark the session as "modified" to make sure it gets saved
+        self.session.modified = True
+
+    def remove(self, product):
+        """
+        Remove a product from the cart.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
 
     def clear(self):
-        """Видалити кошик із сеансу"""
+        # remove cart from session
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
