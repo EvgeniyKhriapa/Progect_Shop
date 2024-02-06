1961764e86e0caf037b24756b99b145d0207fcb6
EvgeniyKhriapa <house28101989@gmail.com>
2024-02-06 17:28:21 +0200

    Create Orders migrate and models2

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -4,12 +4,12 @@ from shop.models import Product
 
 class Order(models.Model):
     """Інформація про клієнта"""
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50, verbose_name='Імя')
+    last_name = models.CharField(max_length=50, verbose_name='Прізвище')
+    email = models.EmailField(verbose_name='Електронна адреса')
+    address = models.CharField(max_length=250, verbose_name='Адреса')
+    postal_code = models.CharField(max_length=20, verbose_name='Поштовий індекс')
+    city = models.CharField(max_length=100, verbose_name='Місто')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
@@ -32,13 +32,13 @@ class OrderItem(models.Model):
     """Зберігає інформацію за кожний оплачений товар"""
     order = models.ForeignKey(Order,
                               related_name='items',
-                              on_delete=models.CASCADE)
+                              on_delete=models.CASCADE, verbose_name='Замовлення')
     product = models.ForeignKey(Product,
                                 related_name='order_items',
-                                on_delete=models.CASCADE)
+                                on_delete=models.CASCADE, verbose_name='Продукт')
     price = models.DecimalField(max_digits=10,
-                                decimal_places=2)
-    quantity = models.PositiveIntegerField(default=1)
+                                decimal_places=2, verbose_name='Ціна')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='Кількість')
 
     def __str__(self):
         return str(self.id)
